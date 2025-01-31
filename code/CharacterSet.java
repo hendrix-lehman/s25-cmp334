502aad74bf2ccdababd72c2b197d6682216e34d7
Hendrix Tavarez <htavarez@gmail.com>
2025-01-31 09:57:47 -0500

    jan30

---
+++ code/CharacterSet.java
@@ -0,0 +1,11 @@
+class CharacterSet {
+  public static void main(String[] args) {
+    char ch = 97;
+    System.out.println("The character is: " + ch);
+
+    long num = Long.MAX_VALUE;
+    System.out.println("The maximum value of long is: " + num);
+    num = num + 1;
+    System.out.println("The maximum value of long is: " + num);
+  }
+}
