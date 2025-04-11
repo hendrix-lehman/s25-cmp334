406fa545a2e184370143bf982a78acae62524a3f
Hendrix Tavarez <htavarez@gmail.com>
2025-04-10 20:52:36 -0400

    apr 10

--- mars/instructions.asm
+++ mars/instructions.asm
@@ -5,17 +5,42 @@
 	.globl __start
 
 __start:
-	li $t0, 2
-	li $t1, 3
-	add $t2, $t0, $t1
+# R-type instructions
+	# the value at register t1 will be the result obtained by adding register t2 and t3
+	add $t1, $t2, $t3 # register addressing
+	
+# I-type instructions
+	# the value at register t1 will be the result obtained by adding register t2 and 124
+	addi $t1, $t2, 124 # immediate addressing
 
+	li $t2, 0x10010000 # register t2 has a valid data segment address 
+	lw $t1, 100($t2) # base addressing
+	
+	beq $t1, $t2, exit # pc-relative addressing
 
+# J-type instructions
+	jal printHello
+	
+	j next # pseudo direct addressing
+	
+next:
+	li $v0, 1
+	addi $a0, $zero, 10
+	syscall
+	
 exit:
 	li $v0, 10
 	syscall
 	
+printHello:
+	li $v0, 4
+	la $a0, hello
+	syscall
+	
+	jr $ra # register direct addressing
 
 ################################################################
 #            data segment                                      #
 ################################################################	
 	.data 
+hello:	.asciiz "Hello, world!\n"
